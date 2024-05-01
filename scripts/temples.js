0b353bdc500e12aca89da32391efb4b4b2c536bd
israeloyeyemi28 <israeloyeyem28@gmail.com>
2024-05-01 17:41:41 +0100

    asfc

--- scripts/temples.js
+++ scripts/temples.js
@@ -3,14 +3,10 @@ const hamButton = document.getElementById("menu");
 const nav = document.querySelector(".navigation");
 const year = document.getElementById("year");
 const lastModified = document.getElementById("lastModified");
-
 const today = new Date();
 const currentYear = today.getFullYear();
-
 year.textContent = currentYear;
-
 const LastModifiedDate = new Date(document.lastModified);
-
 const options = {
   year: "numeric",
   month: "long",
@@ -20,10 +16,8 @@ const options = {
   second: "numeric",
 };
 const formattedDate = LastModifiedDate.toLocaleDateString(undefined, options);
-
 lastModified.textContent = `Last Modified on : ${formattedDate}`;
 lastModified.style.color = "white";
-
 // EVENT LISTENERS
 hamButton.addEventListener("click", function () {
   nav.classList.toggle("open");
