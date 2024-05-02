2b966dfa7b401dec45a4aefc92c83435fc8df6b4
israeloyeyemi28 <israeloyeyem28@gmail.com>
2024-05-02 23:17:51 +0100

    CORRECTIONS TO WEEK " ASSIGNMENT

--- scripts/temples.js
+++ scripts/temples.js
@@ -1,24 +1,13 @@
-// GET DOM ELEMENTS
 const hamButton = document.getElementById("menu");
 const nav = document.querySelector(".navigation");
 const year = document.getElementById("year");
 const lastModified = document.getElementById("lastModified");
 const today = new Date();
-const currentYear = today.getFullYear();
-year.textContent = currentYear;
-const LastModifiedDate = new Date(document.lastModified);
-const options = {
-  year: "numeric",
-  month: "long",
-  day: "numeric",
-  hour: "numeric",
-  minute: "numeric",
-  second: "numeric",
-};
-const formattedDate = LastModifiedDate.toLocaleDateString(undefined, options);
-lastModified.textContent = `Last Modified on : ${formattedDate}`;
+const lastModifiedDate = new Date(document.lastModified);
+year.innerHTML = `${today.getFullYear()}`;
+lastModified.innerHTML = `Last Modified On: ${lastModifiedDate}`;
 lastModified.style.color = "white";
-// EVENT LISTENERS
+
 hamButton.addEventListener("click", function () {
   nav.classList.toggle("open");
   hamButton.classList.toggle("open");
